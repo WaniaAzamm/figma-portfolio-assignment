0e1c9c7196c31ec7ee334680adf312cab6dc169b
Wania Azam <159264679+WaniaAzamm@users.noreply.github.com>
2024-11-28 01:38:59 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,77 +1,76 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { FaArrowDown } from "react-icons/fa";
 
 export default function Page() {
   return (
-    <div className="relative w-[1120px] h-auto mt-[240px] ml-[160px]">
-      <h1 className="w-[360px] h-[129px] font-sans text-[60px] font-medium leading-[60.32px] tracking-[-0.03em] text-left text-gray-800">
+    <div className="relative w-full max-w-[1120px] mx-auto px-4 lg:px-0 h-auto mt-24 lg:mt-56">
+      <h1 className="w-full lg:w-[360px] h-auto text-4xl lg:text-[60px] font-medium leading-tight lg:leading-[60.32px] tracking-[-0.03em] text-left text-gray-800">
         Hello, I’m Wania Azam.
       </h1>
-      <div className="absolute top-[160px] mt-24">
-        <Image height={40} width={40} src="/pic.png" alt="Arrow Icon" />
+      <div className="mt-8 lg:absolute lg:top-[160px]">
+        <FaArrowDown className="w-14 h-10 lg:w-24 lgLh-14"/>
       </div>
-      <div className="absolute top-[64px] ml-[566px] w-[580px] h-[111px]">
-        <p className="font-sans text-3xl leading-[38.44px] tracking-[-0.03em] text-left text-gray-600">
-          A  Developer student who is trying to specialize in Ai, Web 3.0 and Metaverse.
+      <div className="mt-8 lg:absolute  lg:top-[64px] lg:ml-[566px] w-full lg:w-[580px]">
+        <p className="text-base lg:text-3xl leading-normal lg:leading-[38.44px] tracking-[-0.03em] text-left text-gray-600">
+          A Developer student who is trying to specialize in AI, Web 3.0, and Metaverse.
         </p>
       </div>
 
-      <div className="flex w-full h-auto mt-72">
-        <div className="w-[380px] mr-[80px]">
+      <div className="flex flex-wrap lg:flex-nowrap w-full h-auto mt-16 lg:mt-72">
+        <div className="w-full lg:w-[380px] mb-8 lg:mb-0 lg:mr-[80px]">
           <h1 className="text-lg font-medium leading-[21.87px] tracking-[-0.03em] text-gray-800">
             01 / GOVERNOR WEBSITE
           </h1>
-          <p className="text-lg font-medium leading-[21.87px] tracking-[-0.03em] text-gray-700 mt-10">
-            A course enrollment Website to manage course enrollment. have
-            functonality of Apply , Check result and Course Selection
+          <p className="text-base lg:text-lg font-medium leading-normal lg:leading-[21.87px] tracking-[-0.03em] text-gray-700 mt-4">
+            A course enrollment website to manage course enrollment. Includes functionality of Apply, Check Result, and Course Selection.
           </p>
-          <p className="text-base text-gray-700 mt-5">
-            Built with Next.js and Tailwand Css
+          <p className="text-sm lg:text-base text-gray-700 mt-2">
+            Built with Next.js and Tailwind CSS.
           </p>
           <Link
-            className="text-gray-900 underline mt-[150px] block"
+            className="text-gray-900 underline mt-6 block"
             href="https://github.com/WaniaAzamm/course-enrollment-website"
           >
-            View from Github ↗{" "}
+            View from GitHub ↗
           </Link>
         </div>
 
-        <div className="w-[620px] h-[364.68px] bg-blue-500 ml-auto pb-6 relative ">
+        <div className="w-full lg:w-[620px] h-[240px] lg:h-[364.68px] bg-blue-500 relative">
           <Image
-            src="/web1.png"
+            src="/web2.png"
             alt="Project Image 1"
             layout="fill"
             objectFit="cover"
-            className="absolute inset-0 object-cover"
+            className="absolute inset-0"
           />
         </div>
       </div>
-      <hr className="my-16 border-black border-t-2 ml-20" />
+      <hr className="my-16 border-black border-t-2" />
 
-      <div className="flex w-full h-auto mt-32">
-        <div className="w-[380px] mr-[80px]">
+      <div className="flex flex-wrap lg:flex-nowrap w-full h-auto mt-8 lg:mt-32">
+        <div className="w-full lg:w-[380px] mb-8 lg:mb-0 lg:mr-[80px]">
           <h1 className="text-lg font-medium leading-[21.87px] tracking-[-0.03em] text-gray-800">
             02 / PORTFOLIO WEBSITE
           </h1>
-          <p className="text-lg font-medium leading-[21.87px] tracking-[-0.03em] text-gray-700 mt-10">
-            A Portfolio Webiste. have functionality of About me Page, Projects
-            Page, Skills Page and Contact page.
+          <p className="text-base lg:text-lg font-medium leading-normal lg:leading-[21.87px] tracking-[-0.03em] text-gray-700 mt-4">
+            A Ecommerce Furniture website with Amazing and Affordable items.
           </p>
-          <p className="text-base text-gray-700 mt-5">
-            Built with Next.js and Tailwand Css
+          <p className="text-sm lg:text-base text-gray-700 mt-2">
+            Built with Next.js and Tailwind CSS.
           </p>
           <Link
-            className="text-gray-900 underline mt-[150px] block"
-            href="https://github.com/WaniaAzamm/WaniaAzam-Portfolio"
+            className="text-gray-900 underline mt-6 block"
+            href="https://github.com/WaniaAzamm/Furniture-Ecommerce-figma-design"
           >
-            View from Github ↗{" "}
+            View from GitHub ↗
           </Link>
         </div>
 
-        <div className="w-[620px] h-[364.68px] bg-blue-500 ml-auto pb-6 relative">
+        <div className="w-full lg:w-[620px] h-[240px] lg:h-[364.68px] bg-blue-500 relative">
           <Image
-            src="/web2.png"
+            src="/web1.png"
             alt="Project Image 2"
             layout="fill"
             objectFit="cover"
@@ -79,25 +78,28 @@ export default function Page() {
           />
         </div>
       </div>
-      <hr className="my-16 border-black border-t-2 ml-20" />
+      <hr className="my-16 border-black border-t-2" />
 
-      <div className="flex w-full h-auto mt-32">
-        <div className="w-[380px] mr-[80px]">
+      <div className="flex flex-wrap lg:flex-nowrap w-full h-auto mt-8 lg:mt-32">
+        <div className="w-full lg:w-[380px] mb-8 lg:mb-0 lg:mr-[80px]">
           <h1 className="text-lg font-medium leading-[21.87px] tracking-[-0.03em] text-gray-800">
             03 / RESUME BUILDER
           </h1>
-          <p className="text-lg font-medium leading-[21.87px] tracking-[-0.03em] text-gray-700 mt-10">
-            A Resume Builder. have functionality of Generate Resume, Download Resume, Edit Button , Shareable link and Copy Resume Link.
+          <p className="text-base lg:text-lg font-medium leading-normal lg:leading-[21.87px] tracking-[-0.03em] text-gray-700 mt-4">
+            A resume builder with functionalities like Generate Resume, Download Resume, Edit, Shareable Link, and Copy Resume Link.
           </p>
-          <p className="text-base text-gray-700 mt-5">
-           Built with Html, Css and Typescript
+          <p className="text-sm lg:text-base text-gray-700 mt-2">
+            Built with HTML, CSS, and TypeScript.
           </p>
-          <Link className="text-gray-900 underline mt-[150px] block" href="https://github.com/WaniaAzamm/Hackathon-01/tree/main/milestone-05">
-            View from Github ↗{" "}
+          <Link
+            className="text-gray-900 underline mt-6 block"
+            href="https://github.com/WaniaAzamm/Reusme-builder"
+          >
+            View from GitHub ↗
           </Link>
         </div>
 
-        <div className="w-[620px] h-[364.68px] bg-blue-500 ml-auto pb-6 relative">
+        <div className="w-full lg:w-[620px] h-[240px] lg:h-[364.68px] bg-blue-500 relative">
           <Image
             src="/web3.png"
             alt="Project Image 3"
@@ -107,8 +109,7 @@ export default function Page() {
           />
         </div>
       </div>
-      <hr className="my-16 border-black border-t-2 ml-20" />
-      
+      <hr className="my-16 border-black border-t-2" />
     </div>
   );
 }
