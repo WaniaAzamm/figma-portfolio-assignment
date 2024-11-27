99c38698650278247a8fc88ca22af34bb22f598f
Wania Azam <159264679+WaniaAzamm@users.noreply.github.com>
2024-11-28 01:41:20 +0500

    Update Footer.tsx

--- app/component/Footer.tsx
+++ app/component/Footer.tsx
@@ -1,22 +1,22 @@
-import Link from 'next/link';
-import React from 'react';
+import Link from 'next/link'
+import React from 'react'
 
-export default function Footer() {
+function Footer() {
   return (
-    <div className=" py-8 mb-7" > 
-      <div className="flex justify-between items-center"> 
-        <h1 className="text-blue-700 font-sans font-medium text-[21px] leading-[25.2px] tracking-[0.03em] pl-44">
-          Wania Azam-2024
-        </h1>
-        <ul className="flex space-x-4 mr-20 font-sans font-medium text-[21px] leading-[25.2px] tracking-[0.03em] pl-44">
-          <li>
-            <Link href="https://github.com/WaniaAzamm">Github</Link>
-          </li>
-          <li>
-            <Link href="https://www.linkedin.com/in/waniaa-azam/">Linkedin</Link>
-          </li>
-        </ul>
-      </div>
+    <div className='h-[100px] justify-center p-6 xl:p-0 lg:mt-20 sm:mt-4 mt-8 lg:mb-10 mb-2 flex'>
+        <div className='lg:w-[1120px] w-full lg:flex space-y-2 lg:space-y-0 lg:justify-between items-end'>
+           <div className='  font-medium lg:text-[21px] sm:text-[18px] text-[15px] text-center text-blue-900'>
+           Wania Azam - 2024
+           </div> 
+           <div className='flex justify-evenly lg:gap-7 gap-2'>
+            <Link href={'https://github.com/WaniaAzamm'} target='_blank' className=' font-medium text-[14px] sm:text-[16px] lg:text-[21px]'> <p>Github</p> </Link>
+            <Link href={'https://www.linkedin.com/in/waniaa-azam/'} target='_blank' className=' font-medium text-[14px] sm:text-[16px] lg:text-[21px]'> <p>LinkedIn</p> </Link>
+            <Link href={'/'} className=' font-medium text-[14px] sm:text-[16px] lg:text-[21px]'> <p>Twitter</p> </Link>
+            </div> 
+        </div>
+     
     </div>
-  );
+  )
 }
+
+export default Footer
